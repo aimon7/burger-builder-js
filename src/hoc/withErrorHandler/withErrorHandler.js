7d7066a33404c2f507335750f28d0be3b64d302d
aimon7 <air.ioannis@gmail.com>
2021-02-28 23:09:43 +0200

    Removing Old Interceptors

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -4,21 +4,24 @@ import Modal from '../../components/UI/Modal/Modal';
 
 const withErrorHandler = (WrappedComponent, axios) => {
     return class extends Component {
-        constructor(){
-            super();
-            console.log('witheErrorHandler Constructor called...');
+        state = {
+            error: null
+        }
+
+        componentWillMount() {
             this.reqInterceptor = axios.interceptors.request.use(req => {
                 this.setState({error: null});
                 return req;
-            } );
+            });
 
-            this.respInterceptor = axios.interceptors.response.use(response => response, error => {
-                this.setState({error: error});
+            this.resInterceptor = axios.interceptors.response.use(res => res, error => {
+                this.setState({error: error.message});
             });
         }
 
-        state = {
-            error: null
+        componentWillUnmount() {
+            axios.interceptors.request.eject(this.reqInterceptor);
+            axios.interceptors.response.eject(this.resInterceptor);
         }
 
         errorConfirmedHandler = () => {
