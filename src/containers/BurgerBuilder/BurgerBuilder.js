a201e016baec53814f4d5d3960d0e409c2ca9ea7
aimon7 <air.ioannis@gmail.com>
2021-03-23 23:00:02 +0200

    Redux & UI State

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -19,7 +19,6 @@ class BurgerBuilder extends Component {
     // }
 
     state = {
-        purchasable: false,
         purchasing: false,
         loading: false,
         error: false,
@@ -47,7 +46,8 @@ class BurgerBuilder extends Component {
                 return sum + el;
             }, 0);
 
-        this.setState({purchasable: sum > 0})
+        // this.setState({purchasable: sum > 0})
+        return sum > 0;
     }
 
     purchaseHandler = () => {
@@ -95,7 +95,7 @@ class BurgerBuilder extends Component {
                         ingredientRemoved={this.props.onIngredientRemoved}
                         ordered={this.purchaseHandler}
                         price={this.props.price}
-                        purchasable={this.state.purchasable}
+                        purchasable={this.updatePurchaseState(this.props.ings)}
                     />
                 </Aux>
             );
