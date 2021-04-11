5a644c6f57427383b9400b881e645e2290ac9a04
aimon7 <air.ioannis@gmail.com>
2021-04-11 23:15:29 +0300

    Fixing Connect & Routing errors

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -81,11 +81,11 @@ export const authCheckState = () => {
             return dispatch(logout());
 
         const expirationDate = new Date(localStorage.getItem('expirationDate'));
-        if (expirationDate > new Date())
+        if (expirationDate <= new Date())
             return dispatch(logout());
 
         const userId = localStorage.getItem('userId');
         dispatch(authSuccess(token, userId));
-        dispatch(checkAuthTimeout(expirationDate.getSeconds() - new Date().getSeconds()));
+        dispatch(checkAuthTimeout((expirationDate.getTime() - new Date().getTime()) / 1000));
     }
 }
