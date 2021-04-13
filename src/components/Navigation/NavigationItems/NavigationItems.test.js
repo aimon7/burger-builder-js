6ae6a1104e7cd9fcdfb063a4ae3da7c9259632df
aimon7 <air.ioannis@gmail.com>
2021-04-13 21:51:10 +0300

    Testing Components continue

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -8,10 +8,17 @@ import NavigationItem from './NavigationItem/NavigationItem';
 configure({adapter: new Adapter()})
 
 describe('<NavigationItems />', () => {
-    it('should render 2 <NavigationItem /> elements if not authenticated', () => {
-        const wrapper = shallow(<NavigationItems/>);
+    let wrapper;
+    beforeEach(() => {
+       wrapper = shallow(<NavigationItems />);
+    });
 
-        // This means that we expect to render 2 NavigationItem
+    it('should render 2 <NavigationItem /> elements if not authenticated', () => {
         expect(wrapper.find(NavigationItem)).toHaveLength(2);
+    });
+
+    it('should render 3 <NavigationItem /> elements if authenticated', () => {
+        wrapper.setProps({isAuthenticated: true});
+        expect(wrapper.find(NavigationItem)).toHaveLength(3);
     });
 });
